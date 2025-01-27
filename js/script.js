a3f729cede0004e93962b8520348cd76db483e26
Pabluoff <gabriel999pablo@gmail.com>
2025-01-27 16:56:52 -0300

    .

--- js/script.js
+++ js/script.js
@@ -685,7 +685,7 @@ function handleShare(video) {
   </div>
   <div class="share-options">
     <button class="share-option" data-platform="save">
-      <div class="share-icon-wrapper" style="background: #FF3B5C">
+      <div class="share-icon-wrapper" style="background:rgb(81, 81, 81)">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
           <path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"></path>
           <polyline points="17 21 17 13 7 13 7 21"></polyline>
