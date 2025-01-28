1072d1280126bd1cdf1e5b793638f68c2d311e0b
Pabluoff <gabriel999pablo@gmail.com>
2025-01-28 18:06:30 -0300

    plius

--- js/script.js
+++ js/script.js
@@ -150,12 +150,8 @@ function createVideoElement(video, isShared = false) {
     </div>
   </div>
   <div class="video-actions">
-           <button class="add-button">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <path d="M5 12h14" />
-                        <path d="M12 5v14" />
-                    </svg>
+           <button class="follow-button">
+                    seguir
            </button>
             <div class="user-profile">
                 <img src="${video.userProfile}" alt="Profile">
