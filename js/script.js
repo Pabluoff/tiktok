b4672d5452b26bcab967f08437978005277b003f
Pabluoff <gabriel999pablo@gmail.com>
2025-01-28 21:58:26 -0300

    follow-button

--- js/script.js
+++ js/script.js
@@ -151,7 +151,11 @@ function createVideoElement(video, isShared = false) {
   </div>
   <div class="video-actions">
            <button class="follow-button">
-                    seguir
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M5 12h14" />
+                        <path d="M12 5v14" />
+                    </svg>
            </button>
             <div class="user-profile">
                 <img src="${video.userProfile}" alt="Profile">
